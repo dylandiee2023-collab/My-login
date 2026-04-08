48b978bb62a1587015d4c38cf734dd2df158f87a
dylandiee2023-collab <dylandiee2023@gmail.com>
2026-04-08 18:07:52 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <h2>Enter OTP</h2>
+  <form action="/verify" method="POST">
+    <input name="otp" placeholder="OTP Code"><br>
+    <button type="submit">Verify</button>
+  </form>
+</body>
+</html>
